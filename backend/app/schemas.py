b23438d46d4821e83e55f5048038bbbb4a066417
Maryna <maryny.khrystyianyn@gmail.com>
2026-01-18 22:05:45 +0100

    simple backend

---
+++ backend/app/schemas.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+from pydantic import BaseModel
+from datetime import datetime
+
+class NoteBase(BaseModel):
+    title: str
+    content: str
+
+class NoteCreate(NoteBase):
+    pass
+
+class NoteUpdate(NoteBase):
+    pass
+
+class NoteOut(NoteBase):
+    id: int
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
